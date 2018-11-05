ac52217f4f9bbc2b5b22f82e5a289247ffa77a6c
adrianmmartinez <31323115+adrianmmartinez@users.noreply.github.com>
2018-11-05 13:07:15 -0800

    Route purchase array (#1)
    * added comments, made route: /purchase accept an array of ids
    
    * fixed incomplete purchase issue

--- src/main/java/edu/csumb8/grocdb/GroceryController.java
+++ src/main/java/edu/csumb8/grocdb/GroceryController.java
@@ -1,10 +1,10 @@
 package edu.csumb8.grocdb;
 
-
 import edu.csumb8.grocdb.entitites.GroceryItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -13,36 +13,74 @@ public class GroceryController {
     @Autowired
     IGroceryRepository grocRepo;
 
-    @GetMapping ("/getAll")
-    public List<GroceryItem> getAll () {
+    /**
+     * @route /getAll
+     * @return A list of existing groceries in the db
+     */
+    @GetMapping("/getAll")
+    public List<GroceryItem> getAll() {
         List<GroceryItem> result = grocRepo.findAll();
-        System.out.println(result.toString());
         return result;
     }
 
-    @GetMapping ("/id/{id}")
-    public GroceryItem getItemById (@PathVariable String id) {
+    /**
+     * @route /id/{id}
+     * @param String id of item
+     * @return GroceryItem item with matching id
+     */
+    @GetMapping("/id/{id}")
+    public GroceryItem getItemById(@PathVariable String id) {
         GroceryItem item = grocRepo.findByRepoId(id);
         return item;
     }
 
-    @GetMapping ("/available/{id}/{amount}")
-    public boolean checkAvailability (@PathVariable String id, @PathVariable int amount) {
+    /**
+     * @route /available/{id}/{amount}
+     * @param String item id
+     * @param String amount of item to purchase
+     * @return boolean availability
+     */
+    @GetMapping("/available/{id}/{amount}")
+    public boolean checkAvailability(@PathVariable String id, @PathVariable int amount) {
         GroceryItem item = grocRepo.findByRepoId(id);
         boolean canPurchase = item.available(amount);
         return canPurchase;
     }
 
+    /**
+     * @route /purchase
+     * @param String [] ids
+     * @return boolean on succesful purchase
+     */
     @PostMapping("/purchase")
-    public boolean purchase (@RequestParam String id, @RequestParam int amount) {
-        GroceryItem item = grocRepo.findByRepoId(id);
-        if(item == null) {
-            System.out.println("error: cant find item with id = " + id);
-            return false;
+    public boolean purchase(@RequestParam String [] ids) {
+        List<GroceryItem> cart = new ArrayList<>();
+        HashMap<String, GroceryItem> groceryItems = new HashMap<>();
+        for (int i = 0; i<ids.length; i++) {
+            GroceryItem item;
+            String id = ids[i];
+            System.out.println(ids[i]);
+            if (groceryItems.containsKey(id)) {
+                item = groceryItems.get(id);
+            }
+            else {
+                item = grocRepo.findByRepoId(id);
+                groceryItems.put(id, item);
+                if (item == null) {
+                    System.out.println("error: cant find item with id = " + id);
+                    return false;
+                }
+            }
+
+            if (!item.available(1)) {
+                return false;
+            }
+            else {
+                item.purchase(1);
+                cart.add(item);
+            }
         }
-        boolean purchased = item.purchase(amount);
-        System.out.println(item.getItems());
-        grocRepo.save(item);
-        return purchased;
+        grocRepo.saveAll(cart);
+        return true;
     }
 }
